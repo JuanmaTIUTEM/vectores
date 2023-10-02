4ff2e5507a330dd06efe04c18a6d572a53c23ea1
JuanmaTIUTEM <manuel-fernandez@utem-edu.mx>
2023-10-02 11:00:08 -0600

    Iniciando Repo

---
+++ cpostales.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Códigos postales</title>
+</head>
+<!-- Latest compiled and minified CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+
+<body>
+	<div class="d-flex flex-wrap justify-content-center">
+		<section class="container shadow p-4 mb-4 border rounded m-3 bg-light" id="tittle">
+			<h1 class="text-center">Listado de Códigos Postales de México <hr></h1>
+			<div class="d-flex flex-wrap justify-content-center flex-column">
+				<div class="table table-responsive" id="allData">
+					<table class="" id="tbCodes"></table>
+				</div>
+			</div>
+		</section>
+
+		
+	</div>
+</body>
+</html>
