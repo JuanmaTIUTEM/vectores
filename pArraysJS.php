1ffe13f5fb1fb5da038ba5b8705db99d927a3ea3
Peterb75 <a20220250@utem.edu.mx>
2023-10-04 10:49:15 -0600

    hola

---
+++ pArraysJS.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>All arrays</title>
+</head>
+<body>
+
+	<h1>JavaScript Arrays</h1>
+<h2>The length Property</h2>
+
+<p>The length property returns the length of an array:</p>
+
+<p id="demo"></p>
+
+<script>
+const fruits = ["Banana", "Orange", "Apple", "Mango","a"];
+let size = fruits.length;
+document.getElementById("demo").innerHTML = size;
+</script>
+
+</body>
+</html>
